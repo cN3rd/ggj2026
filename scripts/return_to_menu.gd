224a04370cdda0bb6f967f47142d1d466bd616ea
cN3rd <subs.in.tokyo@gmail.com>
2026-01-31 17:21:23 +0200

    Add stuff

--- scripts/return_to_menu.gd
+++ scripts/return_to_menu.gd
@@ -4,6 +4,5 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 		get_tree().change_scene_to_file("res://menu.tscn")
 
-
 func _on_lose_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
