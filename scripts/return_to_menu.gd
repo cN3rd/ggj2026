dba4dde5fde091e61d962a8e33a31076c661c720
cN3rd <subs.in.tokyo@gmail.com>
2026-01-31 15:56:18 +0200

    Add lose screen

--- scripts/return_to_menu.gd
+++ scripts/return_to_menu.gd
@@ -3,3 +3,7 @@ extends Node2D
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 		get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_lose_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
