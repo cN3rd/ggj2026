78047e5a4725861710d344858d5f83b2551b0c60
cN3rd <subs.in.tokyo@gmail.com>
2026-01-31 11:23:39 +0200

    Add main menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-const MAX_ITEMS = 2
+const MAX_ITEMS = 3
 var selected_item: int = 1
+var credits_shown: bool = false
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("down"):
@@ -19,4 +20,7 @@ func _process(_delta: float) -> void:
 			1:
 				get_tree().change_scene_to_packed(preload("res://main.tscn"))
 			2:
+				# TODO: show credits here
 				get_tree().change_scene_to_packed(preload("res://credits.tscn"))
+			3:
+				get_tree().quit()
